9d196a0cc9fa4b5f7a81e72aed86b95ac925e422
MrMcMuffinsThe3rd <muffin.toughin@gmail.com>
2026-09-12 15:33:05 -0400

    Visualising Vector Addition Exercise complete (Week 1 Slide 28)

--- Ibrahim_Seif_Motion2026/Assets/Scripts/AddVectors.cs
+++ Ibrahim_Seif_Motion2026/Assets/Scripts/AddVectors.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class AddVectors : MonoBehaviour
 {
@@ -14,6 +15,42 @@ public class AddVectors : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Exercise: Week 1 Slide 28
+
+        Vector2 rPLusB = rTransform.position + bTransform.position; //task 1 
+
+        if (Keyboard.current.bKey.isPressed && !Keyboard.current.rKey.isPressed) //task 2
+        {
+            Debug.DrawLine(Vector2.zero, bTransform.position, Color.blue);
+        }
+
+        else if (Keyboard.current.rKey.isPressed && !Keyboard.current.bKey.isPressed)//task 3
+        {
+            Debug.DrawLine(Vector2.zero, rTransform.position, Color.red);
+        }
+
+        else if (Keyboard.current.rKey.isPressed && Keyboard.current.bKey.isPressed) //task 4
+        {
+            Debug.DrawLine(Vector2.zero, rPLusB, Color.magenta);
+        }
+
+        //Prof Solution
+
+        //if (Keyboard.current.rKey.wasPressedThisFrame)
+        //{
+        //    Debug.DrawLine(Vector2.zero, rTransform.position, Color.red);
+        //}
+
+        //if (Keyboard.current.bKey.wasPressedThisFrame)
+        //{
+        //    Debug.DrawLine(Vector2.zero, bTransform.position, Color.blue);
+        //}
+
+        //Vector2 rPlusB = rTransform.position + bTransform.position;
+        //bool wasRandBpressed = Keyboard.current.rKey.wasPressedThisFrame && Keyboard.current.bKey.wasPressedThisFrame;
+        //if (wasRandBpressed == true)
+        //{
+        //    Debug.DrawLine(Vector2.zero, rPlusB, Color.magenta);
+        //}
     }
 }
