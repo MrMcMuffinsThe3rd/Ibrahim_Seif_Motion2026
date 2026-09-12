d3529c696cf226005e6c76416081e3f4e817cb4f
MrMcMuffinsThe3rd <muffin.toughin@gmail.com>
2026-09-12 14:21:08 -0400

    Excercise Setup Complete (Week 1 slide 27)

---
+++ Ibrahim_Seif_Motion2026/Assets/Scripts/AddVectors.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class AddVectors : MonoBehaviour
+{
+    public Transform rTransform;
+    public Transform bTransform;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
