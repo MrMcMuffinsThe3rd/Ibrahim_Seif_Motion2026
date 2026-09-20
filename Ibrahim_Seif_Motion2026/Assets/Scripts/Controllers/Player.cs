f1bd07edcceb6639792328ab15664c3c0891592e
MrMcMuffinsThe3rd <muffin.toughin@gmail.com>
2026-09-20 15:53:48 -0400

    Week 2 Journal Task 2 completed

--- Ibrahim_Seif_Motion2026/Assets/Scripts/Controllers/Player.cs
+++ Ibrahim_Seif_Motion2026/Assets/Scripts/Controllers/Player.cs
@@ -18,16 +18,21 @@ public class Player : MonoBehaviour
 
     void Update()
     {
-        if(Keyboard.current.bKey.wasPressedThisFrame)
+        if(Keyboard.current.bKey.wasPressedThisFrame) //task 1 part a
         {
             SpawnBombAtOffset(bombOffset);
             //SpawnBombAtOffset(new Vector(0,1));
             //SpawnBombAtOffset();
         }
 
-        if (Keyboard.current.tKey.wasPressedThisFrame)
+        if (Keyboard.current.tKey.wasPressedThisFrame) //task 1 part b
         {
             SpawnBombTrail(bombTrailSpacing, numberOfTrailBombs);
+        }
+
+        if (Keyboard.current.rKey.wasPressedThisFrame) //task 2
+        {
+            SpawnBombOnRandomCorner(cornerBombDistance);
         }
 
         if (Keyboard.current.wKey.wasPressedThisFrame)
@@ -60,9 +65,44 @@ public class Player : MonoBehaviour
         }
     }
 
-    void SpawnBombOnRandomCorner(float inDistance)
+    void SpawnBombOnRandomCorner(float inDistance) //using normalisation? inDistance -> think of it like the magnitude of vector
+                                                                        //vector3 RandomCorner -> think of it like the direction of vector
     {
 
+            int randomCorner = Random.Range(0, 4);
+
+            //lets try using the player position as a center point for the bombs spawning
+            // so transform.position is our center point vector
+            //and now we will get the vector positions of the 4 corners around the player relative to the inDistance
+            //lets create a vector2 variable for transform.position first
+
+            Vector2 playerTransform = transform.position;
+
+        if (randomCorner == 0)
+        {
+            //top left corner
+            Vector2 topLeftCorner = playerTransform + new Vector2(-inDistance, inDistance);
+            GameObject spawnedBomb1 = Instantiate(bombPrefab, topLeftCorner, Quaternion.identity);
+        }
+        else if (randomCorner == 1)
+        {
+            //top right corner
+            Vector2 topRightCorner = playerTransform + new Vector2(inDistance, inDistance);
+            GameObject spawnedBomb2 = Instantiate(bombPrefab, topRightCorner, Quaternion.identity);
+        }
+        else if (randomCorner == 2)
+        {
+            //bottom left corner
+            Vector2 bottomLeftCorner = playerTransform + new Vector2(-inDistance, -inDistance);
+            GameObject spawnedBomb3 = Instantiate(bombPrefab, bottomLeftCorner, Quaternion.identity);
+        }
+        else if (randomCorner == 3)
+        {
+            //bottom right corner
+            Vector2 bottomRightCorner = playerTransform + new Vector2(inDistance, -inDistance);
+            GameObject spawnedBomb4 = Instantiate(bombPrefab, bottomRightCorner, Quaternion.identity);
+        }
+        
     }
 
     public void WarpPlayer(Transform target, float ratio)
